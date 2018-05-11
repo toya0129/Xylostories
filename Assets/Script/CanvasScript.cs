c3ac6f6294f988fe3c9a9e9c9b2ea5b937f040e3
toya <toya@Ariel.flabRouter>
2018-05-11 14:48:46 +0900

    first commit

---
+++ Assets/Script/CanvasScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour {
+
+	public CanvasRenderer none;
+	public CanvasRenderer red;
+	public CanvasRenderer yellow;
+	public CanvasRenderer green;
+	public CanvasRenderer purple;
+
+	private bool trigger;
+	private float interval = 0.7f;
+	private float time;
+
+	//点滅用
+//	private float speed = 0.05f;
+//	private float boyNoneAlpha;
+//	private float boyRedAlpha;
+//	private bool boyChange;
+//	private float girlNoneAlpha;
+//	private float girlPurpleAlpha;
+//	private bool girlChange;
+
+	// Use this for initialization
+	void Start () {
+		trigger = false;
+
+		//点滅用
+//		boyNoneAlpha = boyNone.GetComponent<Image> ().color.a;
+//		boyRedAlpha = boyRed.GetComponent<Image> ().color.a;
+//
+//		girlNoneAlpha = boyNone.GetComponent<Image> ().color.a;
+//		girlPurpleAlpha = boyRed.GetComponent<Image> ().color.a;
+//
+//		boyChange = true;
+//		girlChange = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+
+		if (time > interval) {
+			time = 0.0f;
+			trigger = !trigger;
+		}
+
+
+		if (trigger == true) {
+			none.GetComponent<Image> ().color = new Color (255.0f, 255.0f, 255.0f, 0.0f);
+			red.GetComponent<Image> ().color = new Color (255.0f, 255.0f, 255.0f, 1.0f);
+		} else if (trigger == false) {
+			none.GetComponent<Image> ().color = new Color (255.0f, 255.0f, 255.0f, 1.0f);
+			red.GetComponent<Image> ().color = new Color (255.0f, 255.0f, 255.0f, 0.0f);
+		}
+
+
+		//点滅用プログラム
+//		boyNone.GetComponent<Image>().color = new Color (255.0f, 255.0f, 255.0f, boyNoneAlpha);
+//		boyRed.GetComponent<Image> ().color = new Color (255.0f, 255.0f, 255.0f, boyRedAlpha);
+//
+//		girlNone.GetComponent<Image>().color = new Color (255.0f, 255.0f, 255.0f, girlNoneAlpha);
+//		girlPurple.GetComponent<Image> ().color = new Color (255.0f, 255.0f, 255.0f, girlPurpleAlpha);
+//
+//
+//		if (boyChange == true) {
+//			boyNoneAlpha -= speed;
+//			boyRedAlpha = 1.0f - boyNoneAlpha;
+//		} else if(boyChange == false){
+//			boyNoneAlpha += speed;
+//			boyRedAlpha = 1.0f - boyNoneAlpha;
+//		}
+//
+//		if (girlChange == true) {
+//			girlNoneAlpha -= speed;
+//			girlPurpleAlpha = 1.0f - girlNoneAlpha;
+//		} else if(girlChange == false){
+//			girlNoneAlpha += speed;
+//			girlPurpleAlpha = 1.0f - girlNoneAlpha;
+//		}
+//
+//
+//		if ((boyNoneAlpha <= 0.0f) && (boyRedAlpha >= 1.0f) && (boyChange == true)) {
+//			boyChange = false;
+//		} else if ((boyNoneAlpha >= 1.0f) && (boyRedAlpha <= 0.0f) && (boyChange == false)) {
+//			boyChange = true;
+//		}
+//
+//		if ((girlNoneAlpha <= 0.0f) && (girlPurpleAlpha >= 1.0f) && (girlChange == true)) {
+//			girlChange = false;
+//		} else if ((girlNoneAlpha >= 1.0f) && (girlPurpleAlpha <= 0.0f) && (girlChange == false)) {
+//			girlChange = true;
+//		}
+//			
+	}
+}
---
+++ Assets/Script/SerialConnecter.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
+
+
+public class SerialConnecter : MonoBehaviour {
+
+	public delegate void SerialDataReceivedEventHandler(string message);
+	public event SerialDataReceivedEventHandler OnDataReceived;
+
+	public string portName = "/dev/cu.usbmodemFA131";
+	public int bandRate = 57600;
+	private Parity parity = Parity.None;
+	private int dataBits = 8;
+	private StopBits stopBits = StopBits.None;
+	private SerialPort serialPort;
+
+	private Thread thread;
+	private bool isRunning = false;
+
+	private string message;
+	private bool isNewMessageReceived = false;
+
+	public bool close = false;
+
+	void Awake(){
+		DontDestroyOnLoad(this.gameObject);
+		Open ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isNewMessageReceived) {
+			OnDataReceived (message);
+		}
+
+		if (close == true) {
+			close = false;
+			Close ();
+		}
+	}
+
+
+	private void Destroy(){
+		Close ();
+	}
+
+	private void Open(){
+		serialPort = new SerialPort (portName, bandRate, parity, dataBits, stopBits);
+		serialPort.Open ();
+
+		isRunning = true;
+
+		thread = new Thread (Read);
+		thread.Start ();
+	}
+
+	private void Close(){
+		isRunning = false;
+
+		if (thread != null && thread.IsAlive) {
+			thread.Join ();
+		}
+
+		if(serialPort != null && serialPort.IsOpen){
+			serialPort.Close ();
+			serialPort.Dispose ();
+		}
+	}
+
+	private void Read(){
+		while(isRunning && serialPort != null && serialPort.IsOpen){
+			try{
+				if(serialPort.BytesToRead>0){
+					message = serialPort.ReadLine();
+					isNewMessageReceived = true;
+				}
+			}catch(System.Exception e){
+				Debug.LogWarning (e.Message);
+			}
+		}
+	}
+
+	public void Write(string message){
+		try{
+			serialPort.Write(message);
+		}catch(System.Exception e){
+			Debug.LogWarning (e.Message);
+		}
+	}
+}
