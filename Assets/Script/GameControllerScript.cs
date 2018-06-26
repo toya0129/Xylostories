2929bc0882b535317ac6b4a89c0a633d2f758368
toya <toya@Ariel.flabRouter>
2018-06-26 22:01:26 +0900

    change new System

---
+++ Assets/Script/GameControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour {
+
+	private string mainCharacter;
+	private bool[] friendsCharacter = new bool[8] ();
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < 8; i++) {
+			friendsCharacter [i] = false;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool FriendsCharacter{
+		get { return friendsCharacter; }
+		set { friendsCharacter =value;}
+	}
+}
---
+++ Assets/Script/StudyScene/CharacterMoveControllerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveControllerScript : MonoBehaviour {
+
+	public SpriteRenderer Bear;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
