3d06943955c58798e46857f7249988b0e3731c3b
toya0129 <citu15059@g.nihon-u.ac.jp>
2020-06-20 00:36:46 +0900

    doing test

---
+++ Assets/Script/FilterImport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+
+public class FilterImport : MonoBehaviour
+{
+    [DllImport("testlib")]
+    private static extern int Add(int a, int b);
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Add(1, 2));
+	}
+}
+    //librosa
+    //matpotlib
+    //madmon
+    
